7e7e0e8168ef6e19f6145bd5fde1ff856496def1
Gordon Ivy Poole <gpoole@ooze.attlocal.net>
2023-04-29 14:05:43 -0400

    input now resets after entering a command

--- src/InputBox.jsx
+++ src/InputBox.jsx
@@ -1,13 +1,21 @@
-export default InputBox;
+import { useState } from 'react';
 
 function InputBox({ parseInput }) {
+  const [ command, changeCommand ] = useState('');
+
+  function onChange(e) {
+    changeCommand(e.target.value);
+  }
 
   return (<form onSubmit={(e) => {
     e.preventDefault();
     const form = e.target;
     const formData = new FormData(form);
     const formJson = Object.fromEntries(formData.entries());
+    changeCommand('');
     parseInput(formJson.input);}}>
-            <input type="text" className="commandInput" name="input" />
+            <input type="text" className="commandInput" name="input" value={command} onChange={onChange} />
           </form>)
 }
+
+export default InputBox;
